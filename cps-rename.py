dc629b0d5f9174a45b8a96491a02162d84e131eb
Thomas Duchesne <thomas@duchesne.io>
2015-08-13 16:24:34 +0200

    Amélioration des commentaires

--- cps-rename.py
+++ cps-rename.py
@@ -22,7 +22,7 @@
 #               LICENCE PUBLIQUE RIEN À BRANLER
 #                     Version 1, Mars 2009
 #
-# Copyright (C) 2014 Thomas Duchesne
+# Copyright (C) 2014 - 2015 Thomas Duchesne
 #
 # La copie et la distribution de copies exactes de cette licence sont
 # autorisées, et toute modification est permise à condition de changer
@@ -41,56 +41,60 @@ nombre_de_fichiers_a_renommer = 0
 nombre_de_fichiers_a_verifier = 0
 
 def renommage(dossier=".", fichiers_exclus=[]):
-	global nombre_de_fichiers_a_verifier
-	global nombre_de_fichiers_a_renommer
+    global nombre_de_fichiers_a_verifier
+    global nombre_de_fichiers_a_renommer
 
-	fichiers_a_renommer = [fichier for fichier in os.listdir("./" + dossier) if fichier not in fichiers_exclus]
+    fichiers_a_renommer = [fichier for fichier in os.listdir("./" + dossier) if fichier not in fichiers_exclus]
 
-	nombre_de_fichiers_a_renommer = len(fichiers_a_renommer)
+    nombre_de_fichiers_a_renommer = len(fichiers_a_renommer)
 
-	for fichier in fichiers_a_renommer:
-		# La partie intéressante est entre le tout premier tiret et le tout
-		# dernier.
-		tirets = []
-		for position, caractere in enumerate(fichier):
-			if caractere == "-":
-				tirets.append(position)
+    for fichier in fichiers_a_renommer:
+        # La partie intéressante est entre le tout premier tiret et le tout
+        # dernier. On liste la position des tirets dans le nom du fichier pour
+        # découper plus facilement celui-ci.
+        tirets = []
+        for position, caractere in enumerate(fichier):
+            if caractere == "-":
+                tirets.append(position)
 
-		try:
-			# On coupe le nom de fichier entre les deux tirets, on supprime les
-			# espaces en trop et on ajoute l'extension.
-			extension = os.path.splitext(os.path.basename(fichier))[1]
-			nouveau_fichier = fichier[tirets[0] + 1:tirets[-1]].strip() + extension
-			nouveau_fichier = " ".join(nouveau_fichier.split())
-			# Certains titres sont en minuscules, d'autres en majuscules. On
-			# homogénéise tout pour que ce soit plus propre et pratique.
-			nouveau_fichier = nouveau_fichier.lower()
+        try:
+            extension = os.path.splitext(os.path.basename(fichier))[1]
 
-			try:
-				os.rename("./" + dossier + "/" + fichier, "./" + dossier + "/" + nouveau_fichier)
+            # On ne garde que les caractères entre les deux tirets extrêmes et
+            # on ajoute l'extension.
+            nouveau_fichier = fichier[tirets[0] + 1:tirets[-1]].strip() + extension
 
-				# Il reste des tirets dans le nom, c'est peut-être le signe d'un
-				# tiret dans l'espèce d'id aléatoire en fin de nom de fichier.
-				if "-" in nouveau_fichier:
-					print(nouveau_fichier + " comporte encore des tirets.")
-					nombre_de_fichiers_a_verifier += 1
+            # On supprime les espaces multiples.
+            nouveau_fichier = " ".join(nouveau_fichier.split())
 
-			except PermissionError:
-				print("Erreur : vous n'avez pas la permission de rennomer " + fichier)
-			except FireNotFoundError:
-				print("Erreur : " + fichier + " n'existe pas.")
+            nouveau_fichier = nouveau_fichier.lower()
 
-		except IndexError:
-			print("Erreur : " + fichier + " ne peut pas être renommé.")
+            try:
+                os.rename("./" + dossier + "/" + fichier, "./" + dossier + "/" + nouveau_fichier)
+
+                # S'il reste des tirets dans le nom, c'est peut-être le signe
+                # qu'il y a un tiret dans l'espèce d'id aléatoire en fin de nom
+                # de fichier.
+                if "-" in nouveau_fichier:
+                    print(nouveau_fichier + " comporte encore des tirets.")
+                    nombre_de_fichiers_a_verifier += 1
+
+            except PermissionError:
+                print("Erreur : vous n'avez pas la permission de rennomer " + fichier)
+            except FileNotFoundError:
+                print("Erreur : " + fichier + " n'existe pas.")
+
+        except IndexError:
+            print("Erreur : " + fichier + " ne peut pas être renommé.")
 
 if __name__ == "__main__":
-	if len(sys.argv) >= 2:
-		try:
-			renommage(sys.argv[1], sys.argv[2:])
-			print("--------------------------------")
-			print("Fichiers renommés : " + str(nombre_de_fichiers_a_renommer))
-			print("Fichiers à vérifier : " + str(nombre_de_fichiers_a_verifier) + " (" + str(int((nombre_de_fichiers_a_verifier / nombre_de_fichiers_a_renommer) * 100)) + " %)")
-		except NotADirectoryError:
-			print("Erreur : " + sys.argv[1] + " n'est pas un dossier.")
-	else:
-		print("Utilisation : " + os.path.basename(__file__) + " dossier fichier_exclus_1 fichier_exclus_2 ...")
+    if len(sys.argv) >= 2:
+        try:
+            renommage(sys.argv[1], sys.argv[2:])
+            print("--------------------------------")
+            print("Fichiers renommés : " + str(nombre_de_fichiers_a_renommer))
+            print("Fichiers à vérifier : " + str(nombre_de_fichiers_a_verifier) + " (" + str(int((nombre_de_fichiers_a_verifier / nombre_de_fichiers_a_renommer) * 100)) + " %)")
+        except NotADirectoryError:
+            print("Erreur : " + sys.argv[1] + " n'est pas un dossier.")
+    else:
+        print("Utilisation : " + os.path.basename(__file__) + " dossier fichier_exclus_1 fichier_exclus_2 ...")
